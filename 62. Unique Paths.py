e457e38b2c1b6443a839293878d09cea87afb82e
Rouzbeh1797 <75285298+Rouzbeh1797@users.noreply.github.com>
2021-06-23 14:19:37 -0600

    Update 62. Unique Paths.py

--- 62. Unique Paths.py
+++ 62. Unique Paths.py
@@ -1,15 +1,26 @@
-# By Rouzbeh on Dec 12
+# By Rouzbeh on June 23, 2021
 #62. Unique Paths
-# no reference user I just rembered "jaygasht and tarkib". We should have a
-#list with (m-1) down* (n-1) right
+# I recorded a video you can watch bere here is hte solutions:
+# uniquePaths1: This is comnimation ("jaygasht and tarkib" in farsi). 
+# refer to chapter 1 and problem 3.1 mathematics of choice or how to count without counting pdf:
+# https://mathsmartinthomas.files.wordpress.com/2018/08/niven-maths-of-choice.pdf
+# list with (m-1) down* (n-1) right
 # Assume m=4 n=3==> we should have m-1 (3) rights and n-1 (3) down in our list. if we had different type of
 # rigth and down the result was (m-1+n-1)!
 # but they are the same type as "tarkib" so it should be divided by (m-1)! and (n-1)!
-# refer to problem 3.1 mathematics of choice or how to count without counting pdf:
-# https://mathsmartinthomas.files.wordpress.com/2018/08/niven-maths-of-choice.pdf
+# leetcode does not accept it as it take long time to solve unless we put @url_cashe
+
+# uniquePaths2 using @lru_cache
+# uniquePaths3 https://www.youtube.com/watch?v=OL8xlEyqMJE&ab_channel=RickyCho
+# uniquePaths4 https://www.youtube.com/watch?v=gp_wfYhjALw&ab_channel=DEEPTITALESRA
+# DEEPTI TALESRA defines dp definition is wrong (above link), refer to the first comment or
+# my program
+
+
+
 from time import time
 from functools import lru_cache
-def uniquePaths(m, n):
+def uniquePaths1(m, n):
     def factoriel(n):
         if n < 1: return
         if n == 1: return 1
@@ -42,6 +53,15 @@ def uniquePaths3(m, n):
     dfs(0,0)
     return count
 
+def uniquePaths4(col_end, row_end):
+    dp=[[1]*col_end for _ in range(row_end)]
+    for row in range (1,len(dp)):
+        for col in range(1,len(dp[row])):
+            dp[row][col]=...
+
+    return dp[-1][-1]
+
+
 
 
 
@@ -51,4 +71,9 @@ print(time()-a)
 a=time()
 print(uniquePaths2(16,12))
 print(time()-a)
-print(uniquePaths3(3,3))
+a=time()
+print(uniquePaths3(16,12))
+print(time()-a)
+a=time()
+print(uniquePaths4(16,12))
+print(time()-a)
