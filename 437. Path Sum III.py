5d5527e58adb664fa1a0e4345fa5306b6be98f46
Rouzbeh1797 <75285298+Rouzbeh1797@users.noreply.github.com>
2021-06-26 15:38:37 -0600

    Create 437. Path Sum III.py

---
+++ 437. Path Sum III.py
@@ -0,0 +1,25 @@
+# I made a vido on that
+# https://www.youtube.com/watch?v=uZzvivFkgtM&ab_channel=CodeAndCoffee
+# https://www.youtube.com/watch?v=HpqPcpP9uVQ&ab_channel=RickyCho
+
+
+class Solution:
+    def pathSum(self, root: TreeNode, sum: int) -> int:
+        count=0
+        compl=collections.defaultdict(int)
+        compl[0]=1
+        def dfs(node, currentSum=0):
+            nonlocal count,compl
+            if node==None:
+                return
+            currentSum+=node.val          
+            count+=compl[currentSum-sum]            
+            compl[currentSum]+=1
+            dfs(node.left,currentSum)
+            dfs(node.right,currentSum)
+            compl[currentSum]-=1
+        
+        
+        
+        dfs(root)
+        return count
