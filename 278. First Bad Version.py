12b20f82379830322c3464b26ccaee4fc48671ee
Rouzbeh1797 <75285298+Rouzbeh1797@users.noreply.github.com>
2021-06-26 09:07:04 -0600

    Update 278. First Bad Version.py

--- 278. First Bad Version.py
+++ 278. First Bad Version.py
@@ -1,43 +1,18 @@
-"""
-278. First Bad Version
-Dec 9, by Rouzbeh
-Did not use any reference.
-"""
-def isBadVersion(n):
-    if n<5: return False
-    else: return True
-
 class Solution:
-    def firstBadVersion(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
-        if n==1 : return 1
-        if isBadVersion(n) == False: return n
-        if isBadVersion(1) == True: return 1
-        def bSearch(min,max):
-            mid=(min+max)//2
-
-            if isBadVersion(mid) is True:
-                if isBadVersion(mid-1) is False:
-                    return mid
-                else:
-                    max=mid-1
-            else:
-                if isBadVersion(mid +1) is True:
-                    return mid+1
-                else:
-                    min = mid + 1
-            return bSearch(min,max)
-
-        return bSearch(1,n)
-
-
-
-a=Solution()
-print(a.firstBadVersion(11))
-
-
-
-
+    def firstBadVersion(self, n):    
+        if isBadVersion(1)==True:
+            return 1
+        l=2
+        r=n
+        while l<=r: 
+            mid=(l+r)//2           
+            res1=isBadVersion(mid-1)
+            res2=isBadVersion(mid)                                
+            if res1==False and res2==True:
+                return mid         
+            if res1==False and res2==False:
+                l=mid+1
+            if res1==True and res2==True:
+                r=mid-1
+            
+            
