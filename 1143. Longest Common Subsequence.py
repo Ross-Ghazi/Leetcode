f195d4fc18c9cad39d38412590e7b73ba465f272
Rouzbeh1797 <75285298+Rouzbeh1797@users.noreply.github.com>
2021-06-26 10:17:51 -0600

    Update 1143. Longest Common Subsequence.py

--- 1143. Longest Common Subsequence.py
+++ 1143. Longest Common Subsequence.py
@@ -3,6 +3,8 @@
 # second method is buttom up and dynamic programming table
 # https://www.youtube.com/watch?v=ASoaQq66foQ&ab_channel=BackToBackSWE
 # https://www.youtube.com/watch?v=sSno9rV8Rhg&t=1014s&ab_channel=AbdulBari
+# longestCommonSubsequence2 and 3 are the same. However, I added 3 later because it is eaiser to implement and undersantd. 
+# The only difference is how to treat the boundry
 
 class Solution:
     #recursive does not ru for really long string, giving error
@@ -28,6 +30,21 @@ class Solution:
                  else:
                       table[i][j] =max(table[i-1][j],table[i][j-1])
         return table[len(text2)][(len(text1))]
+     #buttom up easier to understand 
+    def longestCommonSubsequence3(self, text1: str, text2: str) -> int:
+        text1="0"+text1
+        text2="1"+text2
+        colMax=len(text1)
+        rowMax=len(text2)
+        table=[[0]*colMax for _ in range(rowMax)]      
+       
+        for i in range(1,rowMax):
+            for j in range(1,colMax):
+                if text1[j]==text2[i]:
+                    table[i][j]=table[i-1][j-1]+1
+                else:
+                    table[i][j]  =max (table[i][j-1], table[i-1][j])
+        return table[-1][-1]
 
 
 
