273934916d25f3ec5cd41064f518711bd09a2f26
Rouzbeh1797 <75285298+Rouzbeh1797@users.noreply.github.com>
2021-07-04 19:58:21 -0600

    Update 121. Best Time to Buy and Sell Stock.py

--- 121. Best Time to Buy and Sell Stock.py
+++ 121. Best Time to Buy and Sell Stock.py
@@ -1,10 +1,28 @@
 #121. Best Time to Buy and Sell Stock
+
+# June 2021
+#By Rouzbeh
+#No refernce used. But here is on link, do what exactly what I did:
+# https://www.youtube.com/watch?v=I7KXD2OGDRQ&ab_channel=SaiAnishMalla
+# another methosd is two pointef:
+# https://www.youtube.com/watch?v=1pkOgXD63yU&ab_channel=NeetCode
+def maxProfit(prices):
+        if len (prices)==1:
+            return 0
+        mini=prices[0]
+        maxProf=0
+        for i in range(1,len(prices)):
+            maxProf=max(maxProf,prices[i]-mini)
+            mini=min(prices[i],mini)         
+        return maxProf
+            
+            
 # Dec 11, 2020
 #By Rouzbeh
 #No refernce used. However to find max subarray I used this link before:
 # https://www.youtube.com/watch?v=kqQnhBljXok&t=5s&ab_channel=RickyCho
 
-def  maxProfit(prices):
+def  maxProfit2(prices):
 
     dif=[0]*(len(prices)-1)
     for i in range(len(prices)-1):
