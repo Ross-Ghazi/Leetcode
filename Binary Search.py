6aa194e0bec7165e1ad0a184a92355bbfeba223c
Rouzbeh1797 <75285298+Rouzbeh1797@users.noreply.github.com>
2021-07-10 13:57:14 -0600

    Create Binary Search.py

---
+++ Binary Search.py
@@ -0,0 +1,35 @@
+# RBinSearch and IBinSearch are Recursive and Iterative Binary Search  
+# If target value is inside A they will retrun it.
+# If target value is not inside A they will retrun previoius and next index of that that
+# If target is less than A[0] it will return (-1,0).
+# If target is more than A[max] it will return (max,max+1).
+
+# Recursive Binary Search  
+def RBinSearch(l,r): 
+    if l>r:
+        return r,l        
+    mid=(l+r)//2
+    if target==A[mid]:
+        return mid    
+    if target<A[mid]:
+        return RBinSearch(l,mid-1)
+    else:
+        return RBinSearch(mid+1,r)
+# Iterative Binary Search , exatly same as Recursive
+def IBinSearch(l,r): 
+    while True:
+        if l>r:
+            return r,l        
+        mid=(l+r)//2
+        if target==A[mid]:
+            return mid
+        if target<A[mid]:
+            r=mid-1
+        else:
+            l=mid+1
+
+
+A=[0,1,2,3,4,5,6,7,8]
+target=5.5
+print(RBinSearch(0,len(A)-1))
+print(IBinSearch(0,len(A)-1))
