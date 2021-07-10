f38c3477d73463e1da963a34329bba238df75b4d
Rouzbeh1797 <75285298+Rouzbeh1797@users.noreply.github.com>
2021-07-10 11:23:27 -0600

    Create 73. Set Matrix Zeroes.py

---
+++ 73. Set Matrix Zeroes.py
@@ -0,0 +1,39 @@
+
+# no refernce
+# for zero function: I had initally looping through the whole matrix but there is no need to do that
+# we can loop through only the col and row that we are intersted
+# Also fir zero function first I was not had "if matrix[r0][c] !=0" which cause some bugs as it will change zeros to "" and then you will not change its col or rows as it is not 0 anymore
+
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        row=len(matrix)
+        col=len(matrix[0])
+        def zero(r0,c0):  
+            # I had initally looping through the whole matrix but there is no need to do that
+            # we can loop through only the col and row that we are intersted
+            # for ro in range(row):
+            #     for co in range(col):
+            #         if ro==r and matrix[ro][co] !=0 :
+            #              matrix[ro][co]=""
+            #         if co==c and matrix[ro][co] !=0:
+            #             matrix[ro][co] =""
+            for c in range (col):
+                if matrix[r0][c] !=0 :
+                    matrix[r0][c]="" 
+            for r in range (row):
+                if matrix[r][c0] !=0 :
+                    matrix[r][c0]="" 
+                        
+        for r in range(row):
+            for c in range(col):
+                if matrix[r][c]==0:
+                    zero(r,c)
+        print(matrix)
+        for r in range(row):
+            for  c in range(col):
+                if matrix[r][c]=="":
+                    matrix[r][c]=0
+        
