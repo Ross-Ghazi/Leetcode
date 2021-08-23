30843be21812eeb2e64ab40656028257ff9a0770
Rouzbeh1797 <75285298+Rouzbeh1797@users.noreply.github.com>
2021-08-22 23:01:14 -0600

    Update 31. Next Permutation.py

--- 31. Next Permutation.py
+++ 31. Next Permutation.py
@@ -2,9 +2,15 @@
 #31. Next Permutation
 #ref:
 #https://www.youtube.com/watch?v=9xT2Xzlo4i4&t=41s
+#input[1,7,9,9,8,3]
+# step 1: [1,7,9,9,8,3] Find i=2 j=i-1=1
+# step 2:[1,7,3,8,9,9] reverse i and after
+# step 3: [1,8,3,7,9,9] replace 8 and 7
+
 
 class Solution:
     def nextPermutation(self, nums):
+            # step 1:      
             find=False
             for i in (range(len(nums)-1,0,-1)):
                 if nums[i]>nums[i-1]:
@@ -14,8 +20,10 @@ class Solution:
                 return nums.reverse()
             #in Leetcode, nums=.... will not work because it should be changes in palce
             #when you say nums=.... it will create a new address and will not change the  refence values
+            # step 2:
             nums[:]=nums[:i]+nums[:i-1:-1]
             j=i-1
+            # step 3:
             while i>0 and i<len(nums):
                 if nums[i]>nums[j]:
                     nums[i],nums[j]=nums[j],nums[i]
