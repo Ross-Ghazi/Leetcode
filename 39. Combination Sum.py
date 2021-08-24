d83958ce8527cf90cec76d3bbc5fb58a5be6fe39
Rouzbeh1797 <75285298+Rouzbeh1797@users.noreply.github.com>
2021-08-23 22:58:43 -0600

    Update 39. Combination Sum.py

--- 39. Combination Sum.py
+++ 39. Combination Sum.py
@@ -12,5 +12,19 @@ class Solution(object):
                 backtrack(path+[candidates[i]],i,currentSum+candidates[i])
         backtrack([],0,0)
         return res
+            res=[]
+        #----------------second method, for loop is different way
+        def backtracking(nums,path, currentSum):
+            if currentSum>target:
+                return
+            if currentSum==target:
+                res.append(path)
+            
+            for i,n in enumerate (nums):
+                backtracking(nums[i:],path+[n], currentSum+n)        
+        backtracking (candidates, [], 0)          
+        return res
+    
+    
 a=Solution()  
 print(a.combinationSum([2,3,6,7],7))
