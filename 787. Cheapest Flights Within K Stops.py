1b87b7f5182d94dbc0cae4828a139d2ddb25e4c7
Ross Ghazi <75285298+Ross-Ghazi@users.noreply.github.com>
2021-09-22 16:04:39 -0600

    Update 787. Cheapest Flights Within K Stops.py

--- 787. Cheapest Flights Within K Stops.py
+++ 787. Cheapest Flights Within K Stops.py
@@ -1,10 +1,13 @@
-# the concept is simalr to Dijkstra algorithm, https://github.com/Rouzbeh1797/Leetcode/blob/main/Dijkstra%20algorithm%20to%20findshortest.py
-# However, the main difference is we can have only K stops
-# here is one refernece:
+# There are different ways to solve it: Dijkstra, Bellman Ford, DFS, BFS
+
+# 1way is based on Dijkstra algorithm
+# Here is Dijkstra algorithm itself:  https://github.com/Rouzbeh1797/Leetcode/blob/main/Dijkstra%20algorithm%20to%20findshortest.py
+# However, the main difference here, is that we can have only K stops
+# Here is one refernece on youtube which is wrong:
 #https://www.youtube.com/watch?v=IQOG3w4abAg&t=366s&ab_channel=KnowledgeCenter
 # it first describe the algorithm and then do it on both c++ amd python
-# hoeever he does not have "seen". so his algorithm failed on some cases:
-# Using a set for 'seen' will prevent us from finding solutions with a hogher number of stops.
+# however he does not have set for "seen". so his algorithm failed on some cases:
+# Using a set for 'seen' will prevent us from going through the loops. 
 class Solution(object):
     def findCheapestPrice(self, n, flights, src, dst, K):
         # DFS , https://www.youtube.com/watch?v=60RbWlDFsmI&ab_channel=TECHDOSE
@@ -18,6 +21,12 @@ class Solution(object):
                 currentcost,currentvortex,stopes=heapq.heappop(h)
                 if currentvortex==dst:
                     return currentcost
+                # regualr Dijkstra algorithm just checked with seen, but here we may see that before (which would have been wither lower cost)
+                # but previous visit may had more stops. 
+                # we know previous stop was cheaper. If it also had less stops there is not point to continue it.
+                # but if we are there with lower number of stops we will push this one also to the heap.
+                # so in heap we would have both of them. But we save current number of stops which is the lowest.
+                # so in futrure if we visist this note again, we know its price is higher, but we check its number of stops with current number of stops.                
                 if currentvortex in seen and seen[currentvortex]<=stopes:
                     continue
                 seen[currentvortex] = stopes       
@@ -27,3 +36,7 @@ class Solution(object):
                         heapq.heappush(h,(adjcost+currentcost,adjvor,stopes+1))
         return -1
                 
+   
+
+        
+        
