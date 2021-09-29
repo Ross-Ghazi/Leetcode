23f20bfb4119ec9587d215b4d914f53bf4840301
Ross Ghazi <75285298+Ross-Ghazi@users.noreply.github.com>
2021-09-28 22:24:35 -0600

    Create 97. Interleaving String.py

---
+++ 97. Interleaving String.py
@@ -0,0 +1,28 @@
+class Solution:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        row=len(s1)+1
+        col=len(s2)+1
+        
+        if col+row-2!=len(s3):
+            return False       
+            
+        dp=[[False]*col for _ in range(row)]
+        dp[0][0]=True
+           
+        for r in range(1,row):
+            if dp[r-1][0]==True and s1[r-1]==s3[r-1]:               
+                dp[r][0]=True
+        
+        for c in range(1,col):
+            if dp[0][c-1]==True and s2[c-1]==s3[c-1]:     
+                dp[0][c]=True           
+                
+                
+        for r in range(1,row):
+            for c in range(1,col):
+                if dp[r][c-1]==True and s2[c-1]==s3[r+c-1]:           
+                    dp[r][c]=True
+                if dp[r-1][c]==True and s1[r-1]==s3[r+c-1]:             
+                    dp[r][c]=True       
+        return dp[-1][-1]
+                                
