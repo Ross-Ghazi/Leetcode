a0a47ece410c1acf591791979d870df90bbdc6fb
Ross Ghazi <75285298+Ross-Ghazi@users.noreply.github.com>
2021-09-30 12:37:10 -0600

    Update 9. Palindrome Number.py

--- 9. Palindrome Number.py
+++ 9. Palindrome Number.py
@@ -7,3 +7,23 @@ class Solution:
         else:
             return False
             
+# or
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x<0:
+            return False
+     
+        x1=x
+        inverse=0
+        while True:
+            temp=x%10
+            inverse=inverse*10+temp
+            x=x//10
+            if x==0:
+                break
+        if x1==inverse:
+            return True
+        else:
+            return False 
+            
+            
