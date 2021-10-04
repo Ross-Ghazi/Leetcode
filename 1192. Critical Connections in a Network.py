b7eb0814ec5004078fa136c5dcb7cb33da565308
Ross Ghazi <75285298+Ross-Ghazi@users.noreply.github.com>
2021-10-03 23:16:33 -0600

    Update 1192. Critical Connections in a Network.py

--- 1192. Critical Connections in a Network.py
+++ 1192. Critical Connections in a Network.py
@@ -1,50 +1,38 @@
 # made my own video
-class Solution(object):
-    def criticalConnections(self, n, connections):
-        """
-        :type n: int
-        :type connections: List[List[int]]
-        :rtype: List[List[int]]
-        """
-        graph = collections.defaultdict(list)
-        for v in connections:
-            graph[v[0]].append(v[1])
-            graph[v[1]].append(v[0])
+class Solution:
+    def criticalConnections(self, n: int, connections: List[List[int]]) -> List[List[int]]:
+            graph =defaultdict(list)
+            for n1,n2 in connections:
+                graph[n1].append(n2)
+                graph[n2].append(n1)
+    
             
-        dfn = [None for i in range(n)]
-        low = [None for i in range(n)]        
-   
-        start = 0
-        res = []
-        cur=0
-        seen=set()
-        def dfs(node,parent):
-            nonlocal cur
-            if node  not in seen:
-                dfn[node] = cur
-                low[node] = cur
-                cur+=1
-                seen.add(node)               
-                for n in graph[node]:
-                    if n not in seen:                        
-                        dfs(n,node)
-                    
-                if parent is not None:
-                    l = min([low[i] for i in graph[node] if i!=parent]+[low[node]])
-                else:
-                    # parent is None for first Node.
-                    l = min(low[i] for i in graph[node]+[low[node]])
-                   
-                low[node] = l
+            low=[None]*n
+            rank=[None]*n
+            seen=set()
+            currentrank=0
+            res=[]
+            def dfs(node, parent=None):
+                nonlocal currentrank
+                if node in seen:
+                    return                
                 
-        dfs(0,None)
-
-        
-        for v in connections:
-            if low[v[0]]>dfn[v[1]] or low[v[1]]>dfn[v[0]]:
-            
-                res.append(v)
-        return res
+                rank[node]=currentrank
+                low[node]=currentrank
+                currentrank+=1
+                seen.add(node)
+                
+                for n in graph[node]:
+                    if n not in seen:
+                        dfs(n,node)
+                low[node]=min([low[n] for n in graph[node] if n!=parent]+[low[node]])
+                
+            dfs(0)                    
+            for n1,n2 in connections:
+                if low[n1]>rank[n2] or low[n2]>rank[n1]:
+                    res.append([n1,n2])
+            return res
+    
 a=Solution()
 n=5
 connections = [[0,1],[1,2],[2,0],[1,3]]
