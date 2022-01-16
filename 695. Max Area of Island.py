163c78367170c2e5a93e068fc25f95a507eef762
Ross Ghazi <75285298+Ross-Ghazi@users.noreply.github.com>
2022-01-15 19:54:29 -0700

    Update 695. Max Area of Island.py

--- 695. Max Area of Island.py
+++ 695. Max Area of Island.py
@@ -1,24 +1,26 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        res=0
         row=len(grid)
         col=len(grid[0])
-        area=[0]
-        def con(r,c):
-            if r>=row or r<0 or c>=col or c<0 or grid[r][c]==0:
-                return                
-            area[0]+=1
-            grid[r][c]=0
-            con(r+1,c)
-            con(r-1,c)
-            con(r,c+1)
-            con(r,c-1)
+        res=0
+        
+        def dfs(r,c):
+            nonlocal res,n
+            
+            if c<0 or r<0 or c>=col or r>=row or grid[r][c]!=1:
+                return
+            n+=1            
+            grid[r][c]=0                       
+            dfs(r-1,c)
+            dfs(r+1,c)
+            dfs(r,c-1)
+            dfs(r,c+1)            
+            
         for r in range(row):
             for c in range(col):
                 if grid[r][c]==1:
-                    area[0]=0
-                    con(r,c)
-                    res=max(res,area[0])
+                    n=0
+                    dfs(r,c)
+                    res=max(res,n)                     
         return res
-                
             
