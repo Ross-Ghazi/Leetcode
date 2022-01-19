ce4880f5d325fea381d0f9e42fde301b9a472131
Ross Ghazi <75285298+Ross-Ghazi@users.noreply.github.com>
2022-01-18 19:28:53 -0700

    Update 482. License Key Formatting.py

--- 482. License Key Formatting.py
+++ 482. License Key Formatting.py
@@ -1,21 +1,27 @@
 class Solution:
     def licenseKeyFormatting(self, s: str, k: int) -> str:
-        res=0
-        def dfs(root):
-            if root==None:
-                return 0
-            cur=root
-            l,r=0,0
-            while cur!=0:
-                cur=cur.left
-                l+=1
-            cur=root
-            while cur!=0:
-                cur=cur.
-                r+=1
-            if r==l:                
-                res+=(2**l)-1
-                
-        dfs(root)
-        return res
-    
+        s=s.upper()
+        s="".join(s.split("-"))
+               
+        left=len(s)%k                   
+        s1=s[:left]
+        s=s[left:]
+        
+        if len(s1)>0:
+            s1+="-"
+        group_number=len(s)//k
+        
+        s2=""
+        print(s) 
+        for i in range(group_number):
+            start=i*k
+            end=(i+1)*(k)        
+            temp=s[start:end]
+            s2+=temp+"-"           
+       
+        
+        s3=s1[:]+s2[:]
+        return s3[:-1]
+        
+        
+            
