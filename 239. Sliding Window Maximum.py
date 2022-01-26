595d458c4d3180ce7b6f7740b91681ba37787d52
Ross Ghazi <75285298+Ross-Ghazi@users.noreply.github.com>
2022-01-25 20:21:12 -0700

    Update 239. Sliding Window Maximum.py

--- 239. Sliding Window Maximum.py
+++ 239. Sliding Window Maximum.py
@@ -3,16 +3,16 @@ class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
         if k>=len(nums):
             return [max(nums)]
-        maxi=deque()        
         res=[]
+        q=deque()
         l=0
-        for i in range(len(nums)):
-            while maxi and nums[maxi[-1]]<nums[i]:
-                maxi.pop()
-            maxi.append(i)          
-            if i>=k-1:                
-                while maxi and maxi[0]<l:
-                    maxi.popleft()
-                res+=[nums[maxi[0]]]
-                l+=1
+        for i,n in enumerate(nums):
+            while len(q)>0 and q[-1][1]<n:
+                q.pop()            
+            q.append([i,n])            
+            if q[0][0]<i-k:
+                q.popleft()           
+            if i+1>=k: # we want the sliding winodow length to be k or grater. if index is i., len is i+1
+                res.append(q[0][1])
         return res
+                
