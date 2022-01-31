e0e4817f5d4c258586144736807d42cb63df6fc1
Ross Ghazi <75285298+Ross-Ghazi@users.noreply.github.com>
2022-01-30 22:34:49 -0700

    Update 160. Intersection of Two Linked List.py

--- 160. Intersection of Two Linked List.py
+++ 160. Intersection of Two Linked List.py
@@ -21,3 +21,25 @@ class Solution:
                     return cur
             
             return None
+
+#     or 
+#     class Solution:
+#     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+#         cur1=headA
+#         cur2=headB
+#         set1=set()
+#         set2=set()
+#         while cur1 or cur2:
+#             if cur1:
+#                 if cur1 in set2:
+#                     return cur1
+#                 set1.add(cur1)
+#                 cur1=cur1.next
+            
+#             if cur2:
+#                 if cur2 in set1:
+#                     return cur2
+#                 set2.add(cur2)
+#                 cur2=cur2.next
+        
+#         return None
