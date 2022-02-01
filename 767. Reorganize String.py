d71b6e0e54735f207b201f7b8f3424b9b4726119
Ross Ghazi <75285298+Ross-Ghazi@users.noreply.github.com>
2022-01-31 18:45:52 -0700

    Update 767. Reorganize String.py

--- 767. Reorganize String.py
+++ 767. Reorganize String.py
@@ -1,47 +1,28 @@
-class Solution(object):
-    def reorganizeString(self, s):
-        if len(s)<=1:
-            return ""
-        d1=collections.defaultdict(int)           
-        for i in range(len(s)):
-          d1[s[i]]+=1
-        max1,max2=0,0   
-        c1,c2, res="","",""
-        if len(d1)<=1:
-            return ""
-        while len(d1)>1: 
-            max1,max2=0,0   
-            c1,c2="",""   
-            for key,val in d1.items():
-                if val>max1:
-                   max1=val
-                   c2=c1                   
-                   c1=key
-                elif val>max2:
-                    max2=val
-                    c2=key
-
-            res=res+c1+c2         
-            d1[c1]-=1
-            d1[c2]-=1
-            if d1[c2]<=0:
-                del d1[c2]
-            if d1[c1]<=0:
-                del d1[c1]
-        if len(d1)==0:
-            return res  
+class Solution:
+    def reorganizeString(self, s: str) -> str:
+        freq=collections.Counter(s)
+        h=[]
+        for c,n in freq.items():
+            h.append([-n,c])
+        heapq.heapify(h)
         
-        freq1=list(d1.values())
-        freq=freq1.pop()
-        c1=list(d1.keys())
-        c=c1.pop()
+        prev=None
+        res=""
         
-        if freq>1:
-            return ""
-
-        if c==res[-1]: 
-            return ""
-        for i in range(freq):
-            res+=c
+        while prev or h:            
+            if prev and not h:               
+                return ""                
+            
+            n,c=heapq.heappop(h)
+            n=n*(-1)
+            res+=c           
+            
+            if prev:
+                heapq.heappush(h,prev)
+                prev=None                
+                
+            if n!=1:
+                n-=1
+                prev=[-1*n,c]
         return res
         
