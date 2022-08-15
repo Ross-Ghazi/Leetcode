410c135052d98fdbbcfe9312f403bf67be519391
Ross Ghazi <97241161+RossGhazi@users.noreply.github.com>
2022-08-15 08:19:01 -0600

    Update 743. Network Delay Time.py

--- 743. Network Delay Time.py
+++ 743. Network Delay Time.py
@@ -19,6 +19,26 @@ class Solution:
                     if v2 not in seen:
                         heapq.heappush(minHeap,(w1+w2,v2))
             return res if len(seen)==n else -1
+# bell man ford:
+# see 787. Cheapest Flights Within K Stops,https://leetcode.com/problems/cheapest-flights-within-k-stops/
+
+
+    class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        seen=set()
+        prices=[float("INF")]*n
+        prices[k-1]=0
+        t=0
+        for i in range(n):
+            tempPrices=prices.copy()
+            for s,e,t in times:               
+                tempPrices[e-1]=min(tempPrices[e-1],tempPrices[s-1]+t)
+            prices=tempPrices
+        res=max(prices)
+        return -1 if res==float("INF") else res
+                                  
                 
+                
+                    
                             
                                 
