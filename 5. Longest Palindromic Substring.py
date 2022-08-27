9db2edbe465c46e049e99cc8617f1110f1130f27
Ross Ghazi <97241161+RossGhazi@users.noreply.github.com>
2022-08-27 09:44:11 -0600

    Update 5. Longest Palindromic Substring.py

--- 5. Longest Palindromic Substring.py
+++ 5. Longest Palindromic Substring.py
@@ -1,3 +1,19 @@
+
+# https://leetcode.com/problems/longest-palindromic-substring/discuss/2954/Python-easy-to-understand-solution-with-comments-(from-middle-to-two-ends).
+class Solution(object):
+    def longestPalindrome(self, s):        
+        res = ""
+        for i in range(len(s)):
+            res = max(self.helper(s,i,i), self.helper(s,i,i+1), res, key=len)
+        return res      
+        
+    def helper(self,s,l,r):        
+        while 0<=l and r < len(s) and s[l]==s[r]:
+                l-=1; r+=1
+        return s[l+1:r]
+            
+#######################################################
+#above method is easier.
 # 5. Longest Palindromic Substring
 # Dec 14, 2020
 # ref: https://www.youtube.com/watch?v=y2BD4MJqV20&t=2s&ab_channel=NickWhite
