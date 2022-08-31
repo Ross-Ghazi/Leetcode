07783877ee28c9cc731e97f797112e0e8d3bf602
Ross Ghazi <97241161+RossGhazi@users.noreply.github.com>
2022-08-31 14:49:16 -0600

    Create 1041. Robot Bounded In Circle.py

---
+++ 1041. Robot Bounded In Circle.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isRobotBounded(self, instructions: str) -> bool:
+        s=instructions+instructions+instructions+instructions        
+        ins=[0]*4                
+        index=0
+        for c in s:
+            if c=="G":                
+                ins[index]+=1
+            elif c=="L":
+                index+=1
+            else:
+                index-=1
+            
+            if index>=4:
+                index-=4
+            elif index<0:
+                index+=4
+        if ins[0]==ins[2] and ins[1]==ins[3]:
+            return True
+        return False 
