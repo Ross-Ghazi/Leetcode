4ea796f751c4a22b79dc48bf20adf9a5ee8a69f9
Ross Ghazi <97241161+RossGhazi@users.noreply.github.com>
2022-09-02 10:25:18 -0600

    Update 42. Trapping Rain Water.py

--- 42. Trapping Rain Water.py
+++ 42. Trapping Rain Water.py
@@ -1,29 +1,22 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        if len(height)<=2:
-            return 0
-            
+        #min(max(left),max(right))
         l=0
         r=len(height)-1
-        lMax=height[l]
-        rMax=height[r]
-        res=0
-        while l<r:
-            if lMax<=rMax:            
-                capacity=min(lMax,rMax)-height[l]
-                capacity=max(capacity,0)
-                res+=capacity
+        ans=0
+        maxLeft=height[l]
+        maxRight=height[r]
+        while l<=r:            
+            if maxLeft<maxRight:                
+                area=maxLeft-height[l]
+                ans+=area
                 l+=1
-                lMax=max(lMax,height[l])
-            else:
-                capacity=min(lMax,rMax)-height[r]
-                capacity=max(capacity,0)
-                res+=capacity
-                r-=1
-                rMax=max(rMax,height[r])          
-        return res
-                
-                
-                
+                maxLeft=max(maxLeft,height[l])              
             
+            else:        
+                area=maxRight-height[r]
+                ans+=area
+                r-=1
+                maxRight=max(maxRight,height[r])                             
+        return ans
             
