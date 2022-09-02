7fa39a66ecd9a88517ed2e27a435ab5f219b10fd
Ross Ghazi <97241161+RossGhazi@users.noreply.github.com>
2022-09-02 11:51:32 -0600

    Update 994. Rotting Oranges.py

--- 994. Rotting Oranges.py
+++ 994. Rotting Oranges.py
@@ -1,57 +1,33 @@
-# my ows solution
-# but got some idead from 
-# tps://leetcode.com/problems/rotting-oranges/discuss/563686/Python-Clean-and-Well-Explained-(faster-than-greater-90)
 class Solution:
-    def orangesRotting(self, grid: List[List[int]]) -> int:
-        row=len(grid)
-        col=len(grid[0])
-        
-        time=0
-        counter=[0]
-      
-        # if adjacent cells have fresh orange it will mark them and add to counter
-        #we cannot rotten them direcly becasue the for next cells we do not know 
-        # if they were already rotten or just got rotten
-        def rotten(r0,c0):
-            
-            if r0+1<row and grid[r0+1][c0]==1:
-                grid[r0+1][c0]=""
-                counter[0]+=1
-            if r0-1>=0 and grid[r0-1][c0]==1:
-                grid[r0-1][c0]=""
-                counter[0]+=1    
-
-            if c0+1<col and grid[r0][c0+1]==1:
-                grid[r0][c0+1]=""
-                counter[0]+=1
-            if c0-1>=0 and grid[r0][c0-1]==1:
-                grid[r0][c0-1]=""
-                counter[0]+=1    
-                
-        
-        while True:
-            time+=1
-            counter[0]=0
+     def orangesRotting(self, grid: List[List[int]]) -> int:
+            que=collections.deque()
+            rotten=set()
+            fresh=0
+            row=len(grid)
+            col=len(grid[0])
+            time=0
             for r in range(row):
                 for c in range(col):
-                    if grid[r][c]==2:
-                        rotten(r,c)
-            for r in range(row):
-                for c in range(col):
-                    # change previously rotten orange so we do not  process them again
-                    if grid[r][c]==2:
-                        grid[r][c]="1"
-                    # make marked oranages rotten
-                    if grid[r][c]=="":
-                        grid[r][c]=2
-                    
-            # if there is no new rotten oranage we can exit the loop
-            if counter[0]==0:
-                break     
-
-        for r in range(row):
-             for c in range(col):
                     if grid[r][c]==1:
-                        return -1
-        # time-1 as we looped one extra time to get counter=0
-        return time-1
+                        fresh+=1
+                    elif grid[r][c]==2:
+                        que.append((r,c))
+            
+            direction=[(-1,0),(1,0),(0,1),(0,-1)]
+            while True:             
+                if not fresh:
+                    return time
+                if not que:
+                    return -1                
+                for i in range(len(que)):
+                    r,c=que.popleft()                                        
+                    for x,y in direction:
+                        cc=x+c
+                        rr=y+r
+                        if rr<0 or cc<0 or cc>=col or rr>=row or grid[rr][cc]!=1:
+                            continue                     
+                        grid[rr][cc]=2
+                        fresh-=1
+                        que.append((rr,cc))
+                time+=1
+                
