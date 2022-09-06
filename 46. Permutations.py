5158949f2f5318593773afb6ebaf669423327816
Ross Ghazi <97241161+RossGhazi@users.noreply.github.com>
2022-09-06 00:23:47 -0600

    Update 46. Permutations.py

--- 46. Permutations.py
+++ 46. Permutations.py
@@ -10,6 +10,7 @@ class Solution:
         dp(nums,[])
         return res
     
+    #about two times faster than above
     def permute_fast(self, nums):     
         def dp(cur):          
             res=[]  
@@ -26,3 +27,25 @@ class Solution:
                     res+=[item[:x]+[nums[cur]]+item[x:]]
             return res       
         return dp(0)
+    
+#     or uaing append
+#     class Solution:
+#     def permute(self, nums: List[int]) -> List[List[int]]:
+#         def dp(cur):          
+#             res=[]  
+#             if cur==len(nums)-1:   
+#                     # since this is base case it should have the same format as res wich is [[]]
+#                     res.append([nums[cur]])
+#                     return res             
+#             temp=dp(cur+1)
+#             for item in temp:
+#                 # or:
+#                 # res+=[item[:i]+[nums[cur]]+item[i:] for i in range(len(item)+1)]
+#                 # it is impotant to have len(item)+1 because we want [nums[cur]] 
+#                 #to be be added even after last elelet of the item   
+                
+#                 # is it also important to note that we add [nums[cur]]
+#                 for x in range(len(item)+1):
+#                     res.append(item[:x]+[nums[cur]]+item[x:])
+#             return res       
+#         return dp(0)
