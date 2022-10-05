1371fac3cc6501060e8851801ab92639c01c4eed
Ross Ghazi <97241161+RossGhazi@users.noreply.github.com>
2022-10-05 11:28:26 -0600

    Update 128. Longest Consecutive Sequence.py

--- 128. Longest Consecutive Sequence.py
+++ 128. Longest Consecutive Sequence.py
@@ -1,20 +1,18 @@
 # ref: https://www.youtube.com/watch?v=P6RZZMu_maU&ab_channel=NeetCode
-# on youtube file the for loop is on nums, hower it should be on numsSet to get it faster as there is no duplicat.
+# on youtube file the for loop is on nums, however it should be on numsSet to get it faster as there is no duplicat.
 
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        numsSet=set(nums)
+        numsSet=set(nums)        
         res=0
         # for n in nums: //if we loop on array rather than set
         # it will take more time due to duplicates.
         for n in numsSet:
-            if (n-1) not in numsSet:
-                length=0
-                counter=0
-                while (n+counter) in numsSet:
-                    length+=1
-                    counter+=1
-                res=max(res,length)
+            if n-1 in numsSet:
+                continue       
+            i=0    
+            while (n+i) in numsSet:
+                i+=1
+            res=max(res,i)
         return res
                 
-            
