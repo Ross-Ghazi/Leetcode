b62981e5c3d6c60c21c40da50652dc52eb591ef7
Ross Ghazi <97241161+RossGhazi@users.noreply.github.com>
2022-11-06 09:42:27 -0700

    Update 93. Restore IP Addresses.py

--- 93. Restore IP Addresses.py
+++ 93. Restore IP Addresses.py
@@ -1,5 +1,27 @@
 class Solution:
     def restoreIpAddresses(self, s: str) -> List[str]:
+        res=[]
+        def recursive(sofar,index,dots):
+            if index==len(s) and dots==4:              
+                res.append(sofar[:-1])
+            if index==len(s) or dots==4:
+                return
+            for i in range(index,min(index+3,len(s))):
+                temp=s[index:i+1]
+                if len(temp)>1 and temp[0]=="0":
+                    continue
+                if int(temp)<=255:
+                    recursive(sofar+temp+".",i+1,dots+1)
+        
+        recursive("",0,0)
+        return res
+                
+        
+        
+
+
+class Solution:
+    def restoreIpAddresses(self, s: str) -> List[str]:
         if len(s)>12:
             return
         res = []       
