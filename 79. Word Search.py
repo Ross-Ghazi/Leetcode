3eb1f82c2e8809af28a0c0a6aec8665c572436da
Ross Ghazi <97241161+RossGhazi@users.noreply.github.com>
2022-12-02 13:49:18 -0700

    Update 79. Word Search.py

--- 79. Word Search.py
+++ 79. Word Search.py
@@ -23,11 +23,9 @@ class Solution:
         # lets say the word is "ABCCED":
         # if count("A") in board is more than count("D") then reverse word, so search less 
         if count[word[0]] > count[word[-1]]:
-             word = word[::-1]
+             word = word[::-1]                               
                 
-                
-                
-        def check(r,c,index):
+        def dfs(r,c,index):
             nonlocal flag
             if index>=len(word):
                 flag=True
@@ -38,13 +36,12 @@ class Solution:
             board[r][c]="*"
             dir=[(0,1),(0,-1),(-1,0),(1,0)]            
             for rr,cc in dir:
-                check(r+rr,c+cc,index+1)
+                dfs(r+rr,c+cc,index+1)
             board[r][c]=temp
         for r in range(row):
             for c in range(col):
                 if board[r][c]==word[0]:
-                    check(r,c,0)
+                    dfs(r,c,0)
                     if flag==True:
                         return True
         return False
-                
