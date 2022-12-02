0b035dc937c4cb5b4069a021105fe275992ead73
Ross Ghazi <97241161+RossGhazi@users.noreply.github.com>
2022-12-02 13:48:34 -0700

    Update 79. Word Search.py

--- 79. Word Search.py
+++ 79. Word Search.py
@@ -1,32 +1,50 @@
-# https://www.youtube.com/watch?v=ve2ZMiB1INI&ab_channel=SaiAnishMalla
-class Solution():
-    def exist(self, board, word):
-            row=len(board)
-            col=len(board[0])
-            def check(r,c, index):
-                if r>=row or r<0 or c>=col or c<0:
-                    return False
-                if board[r][c]!=word[index]:
-                    return False
-                if index+1==len(word):
-                    return True
-                index+=1
-                temp=board[r][c]
-                board[r][c]="@"
-                b=check(r+1,c, index) or check(r-1,c, index) or check(r,c+1, index) or check(r,c-1, index)
-                board[r][c]=temp
-                return b
-
-
-            for r in range(row):
-                for c in range(col):
-                    if board[r][c]==word[0]:
-                        bol=check(r,c,0)
-                        if bol==True:
-                             return True
+#https://leetcode.com/problems/word-search/discuss/747144/Python-dfs-backtracking-solution-explained/1531990
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        row=len(board)
+        col=len(board[0])
+        flag=False        
+        if len(word) > row*col:
             return False
-
-a=Solution()
-board=[["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]]
-word= "ABCCED"
-print(a.exist(board,word))
+        
+        # without optimization it will gives time exceed error
+        # perform two optimizations to avoid time exceed error
+        
+        #optimization 1
+        #count of a LETTER in word is Greater than count of it being in board
+        count = Counter(sum(board, []))                
+        for c, countWord in Counter(word).items():
+            if count[c] < countWord:
+                return False
+        
+        #optimization 2        
+        # if count of 1st letter of Word(A) is Greater than that of Last One in Board(B). 
+        # Reverse Search the word then search as less case will be searched.
+        # lets say the word is "ABCCED":
+        # if count("A") in board is more than count("D") then reverse word, so search less 
+        if count[word[0]] > count[word[-1]]:
+             word = word[::-1]
+                
+                
+                
+        def check(r,c,index):
+            nonlocal flag
+            if index>=len(word):
+                flag=True
+                return           
+            if r<0 or c<0 or r>=row or c>=col or board[r][c]!=word[index]:
+                return     
+            temp=board[r][c]
+            board[r][c]="*"
+            dir=[(0,1),(0,-1),(-1,0),(1,0)]            
+            for rr,cc in dir:
+                check(r+rr,c+cc,index+1)
+            board[r][c]=temp
+        for r in range(row):
+            for c in range(col):
+                if board[r][c]==word[0]:
+                    check(r,c,0)
+                    if flag==True:
+                        return True
+        return False
+                
