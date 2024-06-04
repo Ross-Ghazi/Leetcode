2fbc6e98a5a1abf8c51d9cc87b74d4a3abd8672a
Ross Ghazi <97241161+RossGhazi@users.noreply.github.com>
2024-06-04 10:39:18 -0600

    Update 207. Course Schedule.py

--- 207. Course Schedule.py
+++ 207. Course Schedule.py
@@ -1,33 +1,24 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        table={}
-        visited=set()
-        for course in prerequisites:
-            if course[0] not in table:
-                table[course[0]]=[course[1]]
-            else:
-                 table[course[0]]=table[course[0]]+[course[1]]
-        
+        preq=defaultdict(list)
+        for c,p in prerequisites:
+            preq[c].append(p)
+        cycle=set()
+        seen=set()
         def check(course):
-            if course in visited:
-                return False
-            if course not in table:
+            if course in seen:
                 return True
-            
-            visited.add(course)
-            for pre in table[course]:
-                 res=check(pre)
-                 if res == False:
-                    return False
-            visited.remove(course)
-            table[course]=[]
-            return True
-            
-        
-        
-        for course in table:            
-            res=check(course)
-            if res == False:
+            if course in cycle:
                 return False
-                
+            cycle.add(course)
+            for c in preq[course]:
+                if not check(c):
+                    return False
+            cycle.remove(course)
+            seen.add(course)
+            return True
+        for course in range(numCourses):
+            if not check(course):
+                return False
         return True
+
