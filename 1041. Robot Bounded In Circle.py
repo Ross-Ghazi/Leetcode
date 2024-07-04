9238afe2ad3c88d7c28f04028d5adc22efe4bf53
Ross Ghazi <97241161+RossGhazi@users.noreply.github.com>
2024-07-03 22:17:33 -0600

    Update 1041. Robot Bounded In Circle.py

--- 1041. Robot Bounded In Circle.py
+++ 1041. Robot Bounded In Circle.py
@@ -1,5 +1,31 @@
 class Solution:
     def isRobotBounded(self, instructions: str) -> bool:
+        movement=[0,0,0,0]
+        index=0
+        for inst in instructions:
+            if inst=="G":
+                movement[index]+=1
+            elif inst=="R":
+                index+=1
+            else:
+                index-=1
+            if index>3:
+                index-=4
+            elif index<0:
+                index+=4
+        if (movement[0]!=movement[2] or movement[1]!=movement[3]) and index==0:
+            return False
+        return True
+        
+        
+
+
+
+
+
+
+class Solution:
+    def isRobotBounded(self, instructions: str) -> bool:
         s=instructions+instructions+instructions+instructions        
         ins=[0]*4    #index[0]=north, index[1]=east, etc ..
         index=0
