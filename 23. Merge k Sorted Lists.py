337da0db8a13a82bbc77edb747b7ca79ca2ed47e
Ross Ghazi <97241161+RossGhazi@users.noreply.github.com>
2024-07-17 13:11:00 -0600

    Update 23. Merge k Sorted Lists.py

--- 23. Merge k Sorted Lists.py
+++ 23. Merge k Sorted Lists.py
@@ -1,36 +1,35 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
-    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
-        
-        def merge2list(l1,l2):
-            l3=ListNode()
-            temp=l3
-            while l1 and l2:
-                if l1.val<=l2.val:
-                    temp.next=l1
-                    l1=l1.next
-                else:
-                    temp.next=l2
-                    l2=l2.next
-                temp=temp.next
-        
-            if l1:
-                temp.next=l1
-            else:
-                temp.next=l2
-            return l3.next  
-   
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         if len(lists)==0:
-            return None       
-        while len(lists)>1:
-            newList=[]
-            while len(lists)>1:                          
-                l1=lists.pop()
-                l2=lists.pop()
-                newList.append(merge2list(l1,l2))   
-            lists.extend(newList)             
-        return lists.pop()    
+            return None
+        def merge(l1,l2):           
+            cur1=l1
+            cur2=l2
+            dummy=ListNode()
+            cur=dummy
+            while cur1 or cur2:
+                if cur1 and cur2:
+                    if cur1.val<cur2.val:
+                        cur.next=cur1
+                        cur1=cur1.next
+                    else:
+                        cur.next=cur2
+                        cur2=cur2.next                
+                elif cur1:
+                    cur.next=cur1
+                    cur1=cur1.next
+                else:
+                    cur.next=cur2
+                    cur2=cur2.next
+                cur=cur.next
+            return dummy.next      
+       
+        q=collections.deque(lists)
+        while len(q)>1:
+            l1=q.popleft()
+            l2=q.popleft()
+            l3=merge(l1,l2)            
+            q.appendleft(l3)
+        res=q.pop()
+        return res
+        
