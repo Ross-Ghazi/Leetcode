58ecbfb335f5f098c211d4da3c198bbddc809d37
Ross Ghazi <97241161+RossGhazi@users.noreply.github.com>
2024-07-20 08:54:04 -0600

    Update 140. Word Break II.py

--- 140. Word Break II.py
+++ 140. Word Break II.py
@@ -1,6 +1,30 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
         wordDict=set(wordDict)
+        res=[]
+        cur=""
+        def dfs(i):
+            nonlocal cur
+            if i==len(s):
+                cur=cur.strip()
+                res.append(cur)
+                return
+            for j  in range(i,len(s)):
+                word=s[i:j+1]
+                if word in wordDict: 
+                    temp=cur                                 
+                    cur=cur+" "+word                    
+                    dfs(j+1)
+                    cur=temp                  
+
+        dfs(0)
+        return res
+
+# or
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        wordDict=set(wordDict)
         cur=[]
         res=[]
         def helper(i):
