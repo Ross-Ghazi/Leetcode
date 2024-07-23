558d38fbcd8120ec6183d556ca46cf5702303e9e
Ross Ghazi <97241161+RossGhazi@users.noreply.github.com>
2024-07-23 07:09:00 -0600

    Update 45. Jump Game II.py

--- 45. Jump Game II.py
+++ 45. Jump Game II.py
@@ -3,7 +3,7 @@ class Solution:
         l=0
         r=0
         res=0
-        while r<len(nums)-1:
+        while r<len(nums)-1:  # if we are on last element len(nums)-1:), we are done, we do not need to go to loop
             farthest=0
             for i in range(l,r+1):
                farthest=max(farthest,i+nums[i]) 
